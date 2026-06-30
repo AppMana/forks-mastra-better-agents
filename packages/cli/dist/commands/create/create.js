5eedfa85fbd2ecfad6662546b74f995acb3d52c9
doctorpangloss <2229300+doctorpangloss@users.noreply.github.com>
2026-06-30 14:27:24 -0700

    Revert "Include built CLI package artifact"
    This reverts commit 9038186c5ef035dff85e09adf94dc3419c616e7a.

--- packages/cli/dist/commands/create/create.js
+++
@@ -1,7 +0,0 @@
-export { create } from '../../chunk-SIM6KNIQ.js';
-import '../../chunk-W6CPGIEC.js';
-import '../../chunk-4Z57PBTZ.js';
-import '../../chunk-EXG3XKBK.js';
-import '../../chunk-L2SGSIJI.js';
-//# sourceMappingURL=create.js.map
-//# sourceMappingURL=create.js.map
